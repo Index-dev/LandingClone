6d2e13c2869f7a29c8f0049baa7e1ef657464825
EumYoungHyun <opwer032@naver.com>
2021-05-08 07:36:04 +0900

    feat: Add map effect

--- younghyun/src/js/secondPage.js
+++ younghyun/src/js/secondPage.js
@@ -7,15 +7,18 @@ window.addEventListener("wheel", checkSecondScroll);
 
 function checkSecondScroll() {
   const secondTitleContainer = document.querySelector(".secondTitleContainer");
+  const secondMap = document.querySelector(".secondMap");
   const secondTitle = document.querySelector(".secondTitle");
   const secondEffect = document.querySelector(".secondEffect");
   if (checkSecondPage() && !preventScroll_2) {
     preventScrollabout_2(2000);
     secondTitleContainer.classList.remove("secondTitleContainer");
+    secondMap.classList.remove("secondMap");
     secondTitle.classList.remove("secondTitle");
     secondEffect.classList.remove("secondEffect");
     setTimeout(() => {
       secondTitleContainer.className = "secondTitleContainer";
+      secondMap.className = "secondMap";
       secondTitle.className = "secondTitle";
       secondEffect.className = "secondEffect";
     }, 100);
